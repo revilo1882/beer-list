c744cd6e8574da997c24ac0386b6ffc5fc9e076d
Revilo1882 <oliverrharris@gmail.com>
2018-10-11 20:01:06 +0100

    Remove console.log()

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,12 @@ export class App extends Component {
 	}
 
 	componentDidMount() {
-		this.setState({ isLoading: true });
-
 		fetch('https://api.punkapi.com/v2/beers')
 			.then(response => response.json())
       .then(beers => this.setState({ beers }));
   }
 
 	render() {
-		console.log(this.state.beers)
 		return (
 			<div>
         <h1 className='list-header'>Brewdog Beer List</h1>
